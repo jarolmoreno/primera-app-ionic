7b40dde6d75aeb734b3f958e7d4552ed04a61e87
Jarol  Moreno Jimenez <90114389+jarolmoreno@users.noreply.github.com>
2024-08-09 22:06:21 -0500

    Subiendo primer proyecto ionict

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+
+// Call the element loader before the bootstrapModule/bootstrapApplication call
+defineCustomElements(window);
 
 import { AppModule } from './app/app.module';
 
